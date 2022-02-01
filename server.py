15a07b79e4d8d827df654e35f275db4d71ba0b42
renatogm24 <renato.garay@pucp.pe>
2022-01-31 22:52:22 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask  # Importa Flask para permitirnos crear nuestra aplicación
+app = Flask(__name__)    # Crea una nueva instancia de la clase Flask llamada "app"
+@app.route('/')          # El decorador "@" asocia esta ruta con la función inmediatamente siguiente
+def hola_mundo():
+    return 'Hola Mundo!'  # Devuelve la cadena '¡Hola Mundo!' como respuesta
+@app.route('/success')
+def success():
+  return "success"
+
+@app.route('/hola/<name>') # para una ruta '/hola /____' cualquier cosa después de que '/hola/' se pase como una variable 'name'
+def hola(name):
+    print(name)
+    return "Hola, " + name
+
+@app.route('/users/<username>/<id>') # para una ruta '/users/____/____', dos parámetros en la url se pasan como nombre de usuario e id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+if __name__=="__main__":   # Asegúrate de que este archivo se esté ejecutando directamente y no desde un módulo diferente    
+    app.run(debug=True)    # Ejecuta la aplicación en modo de depuración
+
